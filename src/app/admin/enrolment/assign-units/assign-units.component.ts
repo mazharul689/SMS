52f7c6f36c36c0a86e894d870cd6e096ad055d47
Md. Mazharul Islam <majharul689@gmail.com>
2025-03-09 12:27:36 +0600

    assign units updated

--- src/app/admin/enrolment/assign-units/assign-units.component.ts
+++ src/app/admin/enrolment/assign-units/assign-units.component.ts
@@ -43,7 +43,7 @@ export interface allUnits {
   ],
 })
 export class AssignUnitsComponent implements OnInit {
-  displayedColumns: string[] = ['rowID', 'unitCode', 'unitName', 'unitType', 'vetFlag', 'AVETMISS']
+  displayedColumns: string[] = ['rowID', 'ounitCode', 'ounitName', 'ounitType', 'ooutcomeNat', 'ostartDate', 'oendDate']
   dataSource: MatTableDataSource<allUnits>
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator
   @ViewChild(MatSort, { static: true }) sort: MatSort
@@ -57,11 +57,20 @@ export class AssignUnitsComponent implements OnInit {
   setVetFlagVal = []
   setAVETMISSVal = []
   HFormGroup1
+  HFormGroup2
   enrolmentID: any
   userInfo: any
   courseIntakeDateId: any
   courseId: any
   units: any
+  isInitialSelectionDone = false;
+  outcomenational: any
+  trainingData
+  outcome
+  stDate
+  error: any = { isError: false, errorMessage: '' };
+  enDate
+  studentData: any
   constructor(
     private fb: FormBuilder,
     private apiService: ApiService,
@@ -85,6 +94,14 @@ export class AssignUnitsComponent implements OnInit {
       studentEnrolmentId: [this.enrolmentID],
       Rows: this.fb.array([this.newTAarrays()]),
     })
+    this.HFormGroup2 = this.fb.group({
+      userId: [this.userInfo.userid],
+      studentEnrolmentId: [this.enrolmentID],
+      trainingArray: this.fb.array([this.newTAarrays()]),
+    })
+    this.apiService.getAPI('getoutcomenational').subscribe((data) => {
+      this.outcomenational = data['data']
+    })
   }
   isAllSelected() {
     const numSelected = this.selection.selected.length;
@@ -92,9 +109,19 @@ export class AssignUnitsComponent implements OnInit {
     return numSelected === numRows;
   }
   masterToggle() {
-    this.isAllSelected() ?
-      this.selection.clear() :
-      this.dataSource.data.forEach(row => this.selection.select(row));
+    if (!this.isInitialSelectionDone) {
+      // Initially select only rows where statuscheck === 1
+      this.dataSource.data.forEach(row => {
+        if (row.statusCheck === 1) {
+          this.selection.select(row);
+        }
+      });
+      this.isInitialSelectionDone = true; // Mark initial selection as done
+    } else {
+      this.isAllSelected() ?
+        this.selection.clear() :
+        this.dataSource.data.forEach(row => this.selection.select(row));
+    }
   }
   sendSelectedNumbers() {
     let selectedNumbers: number[] = [];
@@ -105,49 +132,124 @@ export class AssignUnitsComponent implements OnInit {
   }
   getUnits(id) {
     this.apiService.getAPI(`getstudentenrolmentbystudentenrolmentid?id=${id}`).subscribe((data) => {
+      this.studentData = data['data'][0]
       this.courseId = data['data'][0].courseid
       this.courseIntakeDateId = data['data'][0].courseintakedateid
       this.apiService.getAPI(`getcourseunitbycourseid?id=${this.courseId}`).subscribe((data) => {
-        this.units = data['data']
-        let unitBody = data['data'];
-        unitBody.sort((a, b) => {
-          if (a.unitorderby < b.unitorderby) {
-            return -1;
+        this.apiService.getAPI(`gettrainingactivity?id=${id}`).subscribe((data1) => {
+          this.trainingData = Array.isArray(data1['data']) ? data1['data'] : [];
+          if (this.trainingData) {
+            this.units = data['data']
+            let unitBody = data['data'];
+            unitBody.sort((a, b) => {
+              if (a.unitorderby < b.unitorderby) {
+                return -1;
+              }
+              if (a.unitorderby > b.unitorderby) {
+                return 1;
+              }
+              return 0;
+            });
+            const rows = this.HFormGroup1.get('Rows') as FormArray;
+            while (rows.length) {
+              rows.removeAt(0);
+            }
+
+            unitBody.forEach((unit, index) => {
+              let matchingTraining = this.trainingData.find(t => t.unitid === unit.unitid) || {};
+
+              let rowData = this.fb.group({
+                rowID: index,
+                trainingActivityId: matchingTraining.trainingactivityid || '',
+                statusCheck: matchingTraining.statuscheck ? 1 : 0,
+                unitId: unit.unitid,
+                unitCode: unit.unitcode,
+                unitName: unit.unitname,
+                unitType: unit.unittype,
+                vetFlag: unit.vetflag,
+                AVETMISS: unit.avetmiss,
+                classSetupId: matchingTraining.classsetupid || null,
+                outcomeNationalId: matchingTraining.outcomenationalid || 9,
+                outcomeTrainingOrgId: matchingTraining.outcomenationalid || 9,
+                startDate: this.datePipe.transform(matchingTraining.startdate || null, 'yyyy-MM-dd'),
+                endDate: this.datePipe.transform(matchingTraining.enddate, 'yyyy-MM-dd'),
+                hoursAttended: matchingTraining.hoursattended || unit.schedulednominalhours
+              });
+
+              rows.push(rowData);
+            });
+
+            console.log(this.HFormGroup1.value);
+            this.dataSource = new MatTableDataSource(this.Rows.value);
+            this.dataSource.paginator = this.paginator;
+            this.dataSource.sort = this.sort;
+            this.masterToggle();
           }
-          if (a.unitorderby > b.unitorderby) {
-            return 1;
+          else {
+            let unitBody = data['data'];
+            unitBody.sort((a, b) => {
+              if (a.unitorderby < b.unitorderby) {
+                return -1;
+              }
+              if (a.unitorderby > b.unitorderby) {
+                return 1;
+              }
+              return 0;
+            });
+            const rows = this.HFormGroup1.get('Rows') as FormArray;
+            while (rows.length) {
+              rows.removeAt(0);
+            }
+
+            unitBody.forEach((unit, index) => {
+
+              let rowData = this.fb.group({
+                rowID: index,
+                trainingActivityId: '',
+                statusCheck: 0,
+                unitId: unit.unitid,
+                unitCode: unit.unitcode,
+                unitName: unit.unitname,
+                unitType: unit.unittype,
+                vetFlag: unit.vetflag,
+                AVETMISS: unit.avetmiss,
+                classSetupId: null,
+                outcomeNationalId: 9,
+                outcomeTrainingOrgId: 9,
+                startDate: this.datePipe.transform(this.studentData.commencementdate, 'yyyy-MM-dd'),
+                endDate: this.datePipe.transform(this.studentData.expectedcompletiondate, 'yyyy-MM-dd'),
+                hoursAttended: unit.schedulednominalhours
+              });
+
+              rows.push(rowData);
+            });
+
+            console.log(this.HFormGroup1.value);
+            this.dataSource = new MatTableDataSource(this.Rows.value);
+            this.dataSource.paginator = this.paginator;
+            this.dataSource.sort = this.sort;
+            this.masterToggle();
           }
-          return 0;
-        });
-        (this.HFormGroup1.get('Rows') as FormArray).removeAt(0);
-        for (let i = 0; i < unitBody.length; i++) {
-          let rowData = this.fb.group({
-            rowID: i,
-            trainingActivityId: '',
-            statusCheck: 0,
-            unitId: unitBody[i].unitid,
-            unitCode: unitBody[i].unitcode,
-            unitName: unitBody[i].unitname,
-            unitType: unitBody[i].unittype,
-            vetFlag: unitBody[i].vetflag,
-            AVETMISS: unitBody[i].avetmiss,
-            classSetupId: null,
-            outcomeNationalId: 9,
-            outcomeTrainingOrgId: null,
-            startDate: this.datePipe.transform(unitBody[i].startdate, 'yyyy-MM-dd'),
-            endDate: this.datePipe.transform(unitBody[i].enddate, 'yyyy-MM-dd'),
-            hoursAttended: unitBody[i].hoursattended
-          });
-          (this.HFormGroup1.get('Rows') as FormArray).push(rowData)
-        }
-        console.log(this.HFormGroup1.value)
-        this.dataSource = new MatTableDataSource() // create new object
-        this.dataSource.data = this.Rows.value
-        this.dataSource.paginator = this.paginator
-        this.dataSource.sort = this.sort
-        this.masterToggle()
+        })
       })
     })
+  }
+  outComeChange(val) {
+    console.log(val)
+    for (let i = 0; i < this.Rows.length; i++) {
+      ((this.HFormGroup1.get('Rows') as FormArray).at(i) as FormGroup).get('outcomeNationalId').patchValue(val);
+    }
+  }
+  sDateChange(val) {
+    for (let i = 0; i < this.Rows.length; i++) {
+      ((this.HFormGroup1.get('Rows') as FormArray).at(i) as FormGroup).get('startDate').patchValue(moment(val));
+    }
+  }
+  eDateChange(val) {
+    this.error = { isError: false, errorMessage: '' }
+    for (let i = 0; i < this.Rows.length; i++) {
+      ((this.HFormGroup1.get('Rows') as FormArray).at(i) as FormGroup).get('endDate').patchValue(moment(val));
+    }
   }
   get Rows(): FormArray {
     return this.HFormGroup1.get("Rows") as FormArray
@@ -234,10 +336,41 @@ export class AssignUnitsComponent implements OnInit {
     let rows = this.sendSelectedNumbers();
     rows.forEach(rowIndex => this.HFormGroup1.value.Rows[rowIndex].statusCheck = 1);
     this.HFormGroup1.value.Rows = this.HFormGroup1.value.Rows.filter(item => item.statusCheck == 1);
-    console.log(this.HFormGroup1.value)
-    this.apiService.postAPI('addtrainingactivity', this.HFormGroup1.value).subscribe((data) => {
-      this.router.navigate(['/admin/enrolment/all-student'])
-    })
+    // console.log(this.HFormGroup1.value)
+    if (this.trainingData) {
+      let editTraning = this.HFormGroup1.value.Rows;
+      console.log('data',editTraning);
+      (this.HFormGroup2.get('trainingArray') as FormArray).removeAt(0);
+      for (let i = 0; i < editTraning.length; i++) {
+        let rowData1 = this.fb.group({
+          trainingActivityId: editTraning[i].trainingActivityId,
+          statusCheck: 1,
+          unitId: editTraning[i].unitId,
+          unitName: editTraning[i].unitName,
+          classSetupId: editTraning[i].classSetupId,
+          outcomeNationalId: editTraning[i].outcomeNationalId,
+          outcomeTrainingOrgId: editTraning[i].outcomeNationalId,
+          startDate: this.datePipe.transform(editTraning[i].startDate, 'yyyy-MM-dd'),
+          endDate: this.datePipe.transform(editTraning[i].endDate, 'yyyy-MM-dd'),
+          hoursAttended: editTraning[i].hoursAttended,
+          unitCode: editTraning[i].unitCode,
+          unitType: editTraning[i].unitType,
+          vetFlag: editTraning[i].vetFlag,
+          AVETMISS: editTraning[i].AVETMISS
+        });
+        (this.HFormGroup2.get('trainingArray') as FormArray).push(rowData1)
+      }
+      console.log('finalcheck',this.HFormGroup2.value)
+      this.apiService.postAPI(`edittrainingactivity?id=${this.enrolmentID}`, this.HFormGroup2.value).subscribe((data) => {
+        this.router.navigate(['/admin/enrolment/all-student'])
+      })
+    }
+    else {
+      this.apiService.postAPI('addtrainingactivity', this.HFormGroup1.value).subscribe((data) => {
+        this.router.navigate(['/admin/enrolment/all-student'])
+      })
+    }
+
   }
   addMoreUnits() {
     let tempDirection;
